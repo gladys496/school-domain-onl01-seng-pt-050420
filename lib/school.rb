e5f1c1476d27e7dcb90ea4eecde4a922ffb202c5
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 00:21:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,25 @@
 class School
+ 
  attr_accessor :name, :roster 
+ 
  def initialize(name)
    @name = name 
  @roster = {}
  end 
+ 
  def roster
    @roster
   end 
+  
   def add_student(name,grade)
   @roster[grade] = [] unless roster[grade]
   @roster[grade] << name
   end
+ 
   def grade(level)
 @roster[level].sort
   end
+
  def sort
     sorted_roster = {}
     self.roster.keys.sort.each do |key|
